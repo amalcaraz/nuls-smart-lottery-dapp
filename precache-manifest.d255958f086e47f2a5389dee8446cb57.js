1cf615a24bffa556d506e682e807bbba339df84e
Angel Manzano <angel.manzano.contractor@bbva.com>
2019-03-16 23:47:49 +0100

    eef75db

---
+++ precache-manifest.d255958f086e47f2a5389dee8446cb57.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "53cbce7df75dad5916cc",
+    "url": "/js/chunk-vendors.d929a97d.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "6bfb08ed7ac9f34a61af",
+    "url": "/css/lottery-detail.4755c822.css"
+  },
+  {
+    "revision": "0f6b68011771f630b6e6",
+    "url": "/css/app.c30a37a7.css"
+  },
+  {
+    "revision": "5d2c61d1fb2bea89beac",
+    "url": "/css/buy-tickets.8cc7aceb.css"
+  },
+  {
+    "revision": "5d2c61d1fb2bea89beac",
+    "url": "/js/buy-tickets.53060e97.js"
+  },
+  {
+    "revision": "53cbce7df75dad5916cc",
+    "url": "/css/chunk-vendors.e60c35e7.css"
+  },
+  {
+    "revision": "3d3ec89207722acef1aa",
+    "url": "/js/about.10fd3e55.js"
+  },
+  {
+    "revision": "0f6b68011771f630b6e6",
+    "url": "/js/app.c479cac6.js"
+  },
+  {
+    "revision": "6bfb08ed7ac9f34a61af",
+    "url": "/js/lottery-detail.0d9aa06e.js"
+  },
+  {
+    "revision": "0ff08549bcd776c4c2d0",
+    "url": "/js/new-lottery.ddb8c90c.js"
+  },
+  {
+    "revision": "76a9b07f925b41caa5d84e19f0c050f3",
+    "url": "/img/logo_nuls.76a9b07f.png"
+  },
+  {
+    "revision": "bdf3815f943c3af537d68d515fb91d8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3ec89207722acef1aa",
+    "url": "/css/about.0f84b7d6.css"
+  }
+];
